40b6f2f0debfe34372b21d2fb7a54f8a30b86c32
杜正昇 <ice_talent@163.com>
2023-07-18 19:38:30 +0800

    踢开Java ASM字节码框架的大门！

---
+++ src/main/java/com/leadboot/gate/proxy/DProxyTest.java
@@ -0,0 +1,26 @@
+package com.leadboot.gate.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * @author: Fred
+ * @date: 2023/7/18 19:35
+ * @description: (类的描述)
+ */
+public class DProxyTest {
+
+    public static void main(String[] args) {
+        MyInterface proxy = MyDynamicProxy.createProxy(MyInterface.class, new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                System.out.println("Before doSomething");
+                // 调用原始对象的方法，如果需要
+                System.out.println("After doSomething");
+                return null;
+            }
+        });
+
+        proxy.doSomething();
+    }
+}
--- src/main/java/com/leadboot/gate/proxy/MyInterface.java
+++ src/main/java/com/leadboot/gate/proxy/MyInterface.java
@@ -8,20 +8,4 @@ import java.lang.reflect.Method;
  */
 public interface MyInterface {
     void doSomething();
-
-    public static void main(String[] args) {
-        MyInterface proxy = MyDynamicProxy.createProxy(MyInterface.class, new InvocationHandler() {
-            @Override
-            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                System.out.println("Before doSomething");
-                // 调用原始对象的方法，如果需要
-                System.out.println("After doSomething");
-                return null;
-            }
-        });
-
-        proxy.doSomething();
-    }
-
-
 }
